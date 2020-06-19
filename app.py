116a7d3acc543809f1b1e13498d7d699ef0924a9
thiagovf1988 <thiagovf@LAPTOP-1FU0JGM9.localdomain>
2020-06-19 09:27:22 +0100

    initialize docker / aws / python / flask / postgresql app

--- app.py
+++ app.py
@@ -176,4 +176,4 @@ def update_graph(xaxis_name, yaxis_name):
                                 hovermode='closest')}
 
 if __name__ == '__main__':
-    app.run_server(debug=False, host='0.0.0.0', port=8020)
+    app.run_server(debug=False, host='0.0.0.0', port=8000)
